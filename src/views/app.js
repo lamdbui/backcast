469bc407419e1145b331434da0e80130c9de5585
Hack Reactor Student <student@hackreactor.com>
2017-09-06 18:28:48 -0700

    Load and render templates for video, list, and search

--- src/views/app.js
+++ src/views/app.js
@@ -4,11 +4,15 @@ var AppView = Backbone.View.extend({
 
   initialize: function() {
     this.videos = new Videos();
+    this.render();
   },
 
 
   render: function() {
     this.$el.html(this.template());
+    this.$el.append((new SearchView({el: this.$el.find('.search')})).render());
+    this.$el.append((new VideoPlayerView({el: this.$el.find('.player')})).render());
+    this.$el.append((new VideoListView({el: this.$el.find('.list')})).render());
     return this;
   },
 
