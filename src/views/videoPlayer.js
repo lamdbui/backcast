860e9efd08606bc0ff9146bd41f5d5019f6a6aea
Hack Reactor Student <student@hackreactor.com>
2017-09-07 13:56:07 -0700

    Set model in video player views

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,13 +1,16 @@
 var VideoPlayerView = Backbone.View.extend({
 
   initialize: function() {
-    this.collection.on('select', function(e) {
-      this.render();
+    this.model = this.collection.at(0);
+    this.model.on('select', function(e) {
+      this.model = e;
+      this.render(e);
     }, this);
   },  
 
-  render: function() {
-    this.$el.html(this.template(this.collection.at(0).attributes));
+  render: function(e) {
+    var currentModel = e || this.model;
+    this.$el.html(this.template(currentModel.attributes));
     return this;
   },
 
