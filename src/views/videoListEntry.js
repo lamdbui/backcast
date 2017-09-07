041d700acd53370caadc039f1f64cbbd833e2bf5
Hack Reactor Student <student@hackreactor.com>
2017-09-07 11:41:27 -0700

    Calls select on model when title is clicked

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,10 +1,17 @@
 var VideoListEntryView = Backbone.View.extend({
 
+  events: {
+    'click': 'select'
+  },
 
   render: function() {
     this.$el.append(this.template(this.model.attributes));
     // this.$el.html(this.template(this.model.attributes));
     return this;
+  },
+
+  select: function() {
+    this.model.select();
   },
 
   template: templateURL('src/templates/videoListEntry.html')
