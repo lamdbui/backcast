15748316bb5d3aa6bb7519ac97fa47ebf2c64540
Hack Reactor Student <student@hackreactor.com>
2017-09-07 11:38:25 -0700

    List should now re-render on sync and add some test code for alternate video list nesting

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,9 +1,16 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function() {
+    this.collection.on('sync', function(e) {
+      this.render();
+    }, this);
+  }, 
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+
+    //$('.list').replaceWith(this.template());
 
     this.$el.find('.video-list').empty();
     this.collection.forEach(this.renderVideoEntry, this);
@@ -11,9 +18,18 @@ var VideoListView = Backbone.View.extend({
     return this;
   },
 
+  // renderVideoEntry: function(video) {
+  //   var listChildren = $('.video-list').children();
+  //   listChildren.forEach(function(child) {
+  //     var videoListEntryView = new VideoListEntryView({el: child, model: video});
+  //     $('.video-list').append(videoListEntryView.render());
+  //   });
+  // },
+
   renderVideoEntry: function(video) {
     var videoListEntry = new VideoListEntryView({el: $('.video-list'), model: video});
-    this.$el.find('.video-list').append(videoListEntry.render());
+    // this.$el.find('.video-list').append(videoListEntry.render());
+    $('.video-list').append(videoListEntry.render());
   },
 
   template: templateURL('src/templates/videoList.html')
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -3,6 +3,7 @@ var VideoListEntryView = Backbone.View.extend({
 
   render: function() {
     this.$el.append(this.template(this.model.attributes));
+    // this.$el.html(this.template(this.model.attributes));
     return this;
   },
 
